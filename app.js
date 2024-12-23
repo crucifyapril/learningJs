9154e5ec676fad25d0dfb0fb315284577bc41810
crucifyapril <sayaf3@yahoo.com>
2024-12-23 19:17:26 +0300

    переменные и константы

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-let num
+// let - переменная, которая может менять свое значение ( ; не обязателен и не несет ошибок в синтаксисе )
+let num = 42
+let firstName = 'Aida'
+// можно менять значение переменной без команды let
+num = 50
+// константы - переменная, которая не может менять свое значение
+const isProgrammer = true
+// функция alert выводит значение в всплывающем окне
+// alert(firstName)
+// console.log - выводит значение в консоль
+console.log(firstName)
+// console.log может принимать несколько значений
+console.log('name:', firstName, isProgrammer, num)
