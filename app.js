ec7512f308d8c7ced1fde13cd4501e0b8e329707
crucifyapril <sayaf3@yahoo.com>
2024-12-23 19:47:27 +0300

    операторы и конкатенация строк

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 // let - переменная, которая может менять свое значение ( ; не обязателен и не несет ошибок в синтаксисе )
+
 let num = 42
 let firstName = 'Aida'
+
 // можно менять значение переменной без команды let
+
 num = 50
+
 // константы - переменная, которая не может менять свое значение
+
 const isProgrammer = true
+
 // функция alert выводит значение в всплывающем окне
+
 // alert(firstName)
-// console.log - выводит значение в консоль
-console.log(firstName)
-// console.log может принимать несколько значений
-console.log('name:', firstName, isProgrammer, num)
+
+// console.log - выводит значение в консоль (Aida)
+
+// console.log(firstName)
+// console.log может принимать несколько значений (name: Aida true 50)
+// console.log('name:', firstName, isProgrammer, num)
+
+// математические операторы в js
+
+// console.log(num + 17)
+// console.log(num - 17)
+// console.log(num * 17)
+// console.log(num / 17)
+
+// присваивание переменных
+
+// let num2 = num + 17
+// console.log(num, num2)
+// num = num2 - num
+// num2 = num2 + 1
+// console.log(num, num2)
+
+// конкатенация в js
+
+const fullName = firstName + ' ' + 'Parks'
